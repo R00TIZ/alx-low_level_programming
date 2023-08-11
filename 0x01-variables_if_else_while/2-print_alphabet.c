2ebbab3102e7ce47b55b9d3fe6a465c0f76fbe5b
R00TIZ <mar.maatouk@gmail.com>
2023-08-11 16:18:06 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Description: print all aplhabet letters
+ *Description: print all aplhabet letters
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char ch = 'a';
