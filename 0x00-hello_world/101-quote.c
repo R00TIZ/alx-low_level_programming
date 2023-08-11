39c08957cb09e13b9d2406205aec96584f9a198a
R00TIZ <mar.maatouk@gmail.com>
2023-08-10 17:52:04 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
-* main - Entry point
-* Return:1
+* main - This program prints a message to the standard error.
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = sizeof(str) - 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
 	return (1);
 }
